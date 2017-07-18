e4adc79f75c6fc2ed96606e7cde3fffe1be6fd56
Blit Softwares <falecom@blitsoft.com.br>
2017-07-18 14:19:00 -0400

    Separando estados e cidades

---
+++ states.sql
@@ -0,0 +1,40 @@
+/*
+-- Query: SELECT 
+uf_code as ibge,
+uf,
+state as name,
+created_at,
+updated_at,
+deleted_at
+FROM blit_secure.cities group by uf
+LIMIT 0, 50000
+
+-- Date: 2017-07-18 14:14
+*/
+INSERT INTO `states` (`ibge`,`uf`,`name`,`created_at`,`updated_at`,`deleted_at`) VALUES (12,'AC','Acre','2017-05-01 00:00:00','2017-05-01 00:00:00',NULL);
+INSERT INTO `states` (`ibge`,`uf`,`name`,`created_at`,`updated_at`,`deleted_at`) VALUES (27,'AL','Alagoas','2017-05-01 00:00:00','2017-05-01 00:00:00',NULL);
+INSERT INTO `states` (`ibge`,`uf`,`name`,`created_at`,`updated_at`,`deleted_at`) VALUES (13,'AM','Amazonas','2017-05-01 00:00:00','2017-05-01 00:00:00',NULL);
+INSERT INTO `states` (`ibge`,`uf`,`name`,`created_at`,`updated_at`,`deleted_at`) VALUES (16,'AP','Amapá','2017-05-01 00:00:00','2017-05-01 00:00:00',NULL);
+INSERT INTO `states` (`ibge`,`uf`,`name`,`created_at`,`updated_at`,`deleted_at`) VALUES (29,'BA','Bahia','2017-05-01 00:00:00','2017-05-01 00:00:00',NULL);
+INSERT INTO `states` (`ibge`,`uf`,`name`,`created_at`,`updated_at`,`deleted_at`) VALUES (23,'CE','Ceará','2017-05-01 00:00:00','2017-05-01 00:00:00',NULL);
+INSERT INTO `states` (`ibge`,`uf`,`name`,`created_at`,`updated_at`,`deleted_at`) VALUES (53,'DF','Distrito Federal','2017-05-01 00:00:00','2017-05-01 00:00:00',NULL);
+INSERT INTO `states` (`ibge`,`uf`,`name`,`created_at`,`updated_at`,`deleted_at`) VALUES (32,'ES','Espírito Santo','2017-05-01 00:00:00','2017-05-01 00:00:00',NULL);
+INSERT INTO `states` (`ibge`,`uf`,`name`,`created_at`,`updated_at`,`deleted_at`) VALUES (52,'GO','Goiás','2017-05-01 00:00:00','2017-05-01 00:00:00',NULL);
+INSERT INTO `states` (`ibge`,`uf`,`name`,`created_at`,`updated_at`,`deleted_at`) VALUES (21,'MA','Maranhão','2017-05-01 00:00:00','2017-05-01 00:00:00',NULL);
+INSERT INTO `states` (`ibge`,`uf`,`name`,`created_at`,`updated_at`,`deleted_at`) VALUES (31,'MG','Minas Gerais','2017-05-01 00:00:00','2017-05-01 00:00:00',NULL);
+INSERT INTO `states` (`ibge`,`uf`,`name`,`created_at`,`updated_at`,`deleted_at`) VALUES (50,'MS','Mato Grosso do Sul','2017-05-01 00:00:00','2017-05-01 00:00:00',NULL);
+INSERT INTO `states` (`ibge`,`uf`,`name`,`created_at`,`updated_at`,`deleted_at`) VALUES (51,'MT','Mato Grosso','2017-05-01 00:00:00','2017-05-01 00:00:00',NULL);
+INSERT INTO `states` (`ibge`,`uf`,`name`,`created_at`,`updated_at`,`deleted_at`) VALUES (15,'PA','Pará','2017-05-01 00:00:00','2017-05-01 00:00:00',NULL);
+INSERT INTO `states` (`ibge`,`uf`,`name`,`created_at`,`updated_at`,`deleted_at`) VALUES (25,'PB','Paraíba','2017-05-01 00:00:00','2017-05-01 00:00:00',NULL);
+INSERT INTO `states` (`ibge`,`uf`,`name`,`created_at`,`updated_at`,`deleted_at`) VALUES (26,'PE','Pernambuco','2017-05-01 00:00:00','2017-05-01 00:00:00',NULL);
+INSERT INTO `states` (`ibge`,`uf`,`name`,`created_at`,`updated_at`,`deleted_at`) VALUES (22,'PI','Piauí','2017-05-01 00:00:00','2017-05-01 00:00:00',NULL);
+INSERT INTO `states` (`ibge`,`uf`,`name`,`created_at`,`updated_at`,`deleted_at`) VALUES (41,'PR','Paraná','2017-05-01 00:00:00','2017-05-01 00:00:00',NULL);
+INSERT INTO `states` (`ibge`,`uf`,`name`,`created_at`,`updated_at`,`deleted_at`) VALUES (33,'RJ','Rio de Janeiro','2017-05-01 00:00:00','2017-05-01 00:00:00',NULL);
+INSERT INTO `states` (`ibge`,`uf`,`name`,`created_at`,`updated_at`,`deleted_at`) VALUES (24,'RN','Rio Grande do Norte','2017-05-01 00:00:00','2017-05-01 00:00:00',NULL);
+INSERT INTO `states` (`ibge`,`uf`,`name`,`created_at`,`updated_at`,`deleted_at`) VALUES (11,'RO','Rondônia','2017-05-01 00:00:00','2017-05-01 00:00:00',NULL);
+INSERT INTO `states` (`ibge`,`uf`,`name`,`created_at`,`updated_at`,`deleted_at`) VALUES (14,'RR','Roraima','2017-05-01 00:00:00','2017-05-01 00:00:00',NULL);
+INSERT INTO `states` (`ibge`,`uf`,`name`,`created_at`,`updated_at`,`deleted_at`) VALUES (43,'RS','Rio Grande do Sul','2017-05-01 00:00:00','2017-05-01 00:00:00',NULL);
+INSERT INTO `states` (`ibge`,`uf`,`name`,`created_at`,`updated_at`,`deleted_at`) VALUES (42,'SC','Santa Catarina','2017-05-01 00:00:00','2017-05-01 00:00:00',NULL);
+INSERT INTO `states` (`ibge`,`uf`,`name`,`created_at`,`updated_at`,`deleted_at`) VALUES (28,'SE','Sergipe','2017-05-01 00:00:00','2017-05-01 00:00:00',NULL);
+INSERT INTO `states` (`ibge`,`uf`,`name`,`created_at`,`updated_at`,`deleted_at`) VALUES (35,'SP','São Paulo','2017-05-01 00:00:00','2017-05-01 00:00:00',NULL);
+INSERT INTO `states` (`ibge`,`uf`,`name`,`created_at`,`updated_at`,`deleted_at`) VALUES (17,'TO','Tocantins','2017-05-01 00:00:00','2017-05-01 00:00:00',NULL);
